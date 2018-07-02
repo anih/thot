fa98eea0fa38d738806ecce369ecaa95345a6ddc
Sebastian Bauer <admin@e-back.net>
2018-07-02 12:16:01 +0200

    Lower memory requrements for LevelDB part 2

--- src/phrase_models/LevelDbPhraseTable.cc
+++ src/phrase_models/LevelDbPhraseTable.cc
@@ -31,9 +31,9 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 LevelDbPhraseTable::LevelDbPhraseTable(void)
 {
     options.create_if_missing = true;
-    options.max_open_files = 4000;
-    options.filter_policy = leveldb::NewBloomFilterPolicy(48);
-    options.block_cache = leveldb::NewLRUCache(100 * 1048576);  // 100 MB for cache
+    options.max_open_files = 20;
+    options.filter_policy = leveldb::NewBloomFilterPolicy(1);
+    options.block_cache = leveldb::NewLRUCache(0.5 * 1048576);  // 100 MB for cache
     db = NULL;
     dbName = "";
 }
